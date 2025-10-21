9274525e5446f9bb3c0d9b161f19ed75e2252e8a
ohowe <ohowe@duck.com>
2025-10-21 12:35:51 -0700

    more github actions like 404

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import ShellPage from '$lib/components/shell_page.svelte';
+	import { page } from '$app/state';
+	import type { InitialCommand } from '$lib/components/shell_content.svelte';
+
+	let pageUrl = page.url.href;
+	// In the case that they have noscript, it will use the prerendered URL so switch that
+	if (pageUrl === "http://sveltekit-prerender/404") {
+		pageUrl = "https://olihowe.com/404";
+	}
+
+	const initialCommands: InitialCommand[] = [{command: `curl -I ${pageUrl}`, forcedResult: "HTTP/2 404: Page Not Found"}, {command: 'echo $HOMEPAGE'}];
+</script>
+
+<svelte:head>
+	<title>404 Not Found - Oliver Howe</title>
+</svelte:head>
+
+<ShellPage {initialCommands} />
