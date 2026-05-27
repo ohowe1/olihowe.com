2b13226c35b4e44844a9c015418a6d6540f94ba1
ohowe <ohowe@duck.com>
2026-05-26 21:15:41 -0600

    fix issue

--- src/lib/components/files/E80.txt.svelte
+++ src/lib/components/files/E80.txt.svelte
@@ -3,14 +3,16 @@
 	let { execute }: { execute: (command: string) => void } = $props();
 </script>
 
-For my experimental engineering class (E80) we built an autonomous vehicle that navigated the ocean
-surface with a sensor package that was lowered on a winch and collected data on the turbidity,
-temperature, and depth. The project involved mechanical design, circuit design, and software
-development.
+<div>
+	For my experimental engineering class (E80) we built an autonomous vehicle that navigated the
+	ocean surface with a sensor package that was lowered on a winch and collected data on the
+	turbidity, temperature, and depth. The project involved mechanical design, circuit design, and
+	software development.
 
-<enhanced:img src={robotImg} alt="E80 robot in the water" class="robot-image" />
+	<enhanced:img src={robotImg} alt="E80 robot in the water" class="robot-image" />
 
-You can read my report <a href="/e80report.pdf" target="_blank">here</a>.
+	You can read my report <a href="/e80report.pdf" target="_blank">here</a>.
+</div>
 
 <style>
 	.robot-image {
--- src/lib/components/files/Sites.txt.svelte
+++ src/lib/components/files/Sites.txt.svelte
@@ -2,7 +2,7 @@
 	let { execute }: { execute: (command: string) => void } = $props();
 </script>
 
-<span>
+<div>
 	Some of my projects are hosted on my site site:
 	<a href="https://sites.olihowe.com/" target="_blank" rel="noopener noreferrer">https://sites.olihowe.com/</a>
-</span>
+</div>
